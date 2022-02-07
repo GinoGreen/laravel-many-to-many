9ab56d312a374d06ee5e738adcc5447e05d812f2
GinoGreen <luigiverde97@gmail.com>
2022-02-07 15:28:10 +0100

    relation many to many created (posts and tags)

--- app/Post.php
+++ app/Post.php
@@ -7,21 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'content', 'slug', 'category_id'];
+   protected $fillable = ['title', 'content', 'slug', 'category_id'];
 
-    public function category() {
-        return $this->belongsTo('App\Category');
-    }
+   public function category() {
+      return $this->belongsTo('App\Category');
+   }
 
-    public static function generateUniqueSlug($string) {
-        $slug = Str::slug($string, '-');
-        $originalSlug = $slug;
-        $foundSlug = Post::where('slug', $slug)->first();
-        $count = 1;
-        while ($foundSlug) {
-            $slug = $originalSlug . '-' . $count++;
-            $foundSlug = Post::where('slug', $slug)->first();
-        }
-        return $slug;
-    }
+   public function tags() {
+      return $this->belongsToMany('App\Tag');
+   }
+
+   public static function generateUniqueSlug($string) {
+      $slug = Str::slug($string, '-');
+      $originalSlug = $slug;
+      $foundSlug = Post::where('slug', $slug)->first();
+      $count = 1;
+      while ($foundSlug) {
+         $slug = $originalSlug . '-' . $count++;
+         $foundSlug = Post::where('slug', $slug)->first();
+      }
+      return $slug;
+   }
 }
